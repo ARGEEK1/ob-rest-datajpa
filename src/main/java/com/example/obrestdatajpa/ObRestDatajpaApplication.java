5962d3df9bc4b1334db2073440b0f490fa790c22
ARGEEK1 <antonio198889@gmail.com>
2023-10-31 01:48:47 +0100

    add methods http

--- src/main/java/com/example/obrestdatajpa/ObRestDatajpaApplication.java
+++ src/main/java/com/example/obrestdatajpa/ObRestDatajpaApplication.java
@@ -18,17 +18,18 @@ public class ObRestDatajpaApplication {
         System.out.println(bookRepository.count());
 
         //crear un libro
-        Book java = new Book(null, "Java", "ames Gosling", 600, 20.50, LocalDate.of(1996, 1, 23), true);
-
+        Book book1 = new Book(null, "Java", "ames Gosling", 600, 20.50, LocalDate.of(1996, 1, 23), true);
+        Book book2 = new Book(null, "Java Avanzado", "ames Gosling", 700, 20.50, LocalDate.of(1996, 1, 23), true);
         // almacenar un libro
-        bookRepository.save(java);
+        bookRepository.save(book1);
+        bookRepository.save(book2);
         System.out.println("Cantidad de libros en DB es: " + bookRepository.count());
 
         // recuperar todos los libros
         System.out.println(bookRepository.findAll().get(0));
 
         // borrar un libro
-        bookRepository.deleteById(1L);
+        //bookRepository.deleteById(1L);
         System.out.println(bookRepository.count());
     }
 
--- src/main/java/com/example/obrestdatajpa/controller/BookController.java
+++ src/main/java/com/example/obrestdatajpa/controller/BookController.java
@@ -1,8 +1,50 @@
 package com.example.obrestdatajpa.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.obrestdatajpa.entities.Book;
+import com.example.obrestdatajpa.repository.BookRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class BookController {
+
+    // Atributos
+    private BookRepository bookRepository;
+
+    // Constructores
+    public BookController(BookRepository bookRepository) {
+       this.bookRepository = bookRepository;
+    }
+
+    // CRUD sobre la entidad Book
+    // Buscar todos los libros en base de datos
+
+    /**
+     * http://localhost:8081/api/books
+     * @return
+     */
+    @GetMapping("/api/books")
+    public List<Book> findAll() {
+        // recuperar y devolver los libros en BD
+        return bookRepository.findAll();
+    }
+
+    // Buscar un solo libro en base de datos, según su ID
+    @GetMapping(path = "/api/books/{id}")
+    public Optional<Book> findById(@PathVariable("id") Long id) {
+        return  bookRepository.findById(id);
+    }
+
+    // Crear un nuevo libro en base de datos
+    @PostMapping("/api/books")
+    public Book save(@RequestBody Book book){
+        return bookRepository.save(book);
+    }
+
+    // Actualizar un libro en base de datos
+
+    // Borrar un libro en base de datos
 
 }

8fef0c28c21da4c979f73dafdf2292a41f73e7bc
ARGEEK1 <antonio198889@gmail.com>
2023-11-02 19:19:01 +0100

    ADD test book controller

---
+++ src/test/java/com/example/obrestdatajpa/controller/BookControllerTest.java
@@ -0,0 +1,59 @@
+package com.example.obrestdatajpa.controller;
+
+import com.example.obrestdatajpa.entities.Book;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class BookControllerTest {
+
+    // crear un objeto rest template builder, para poder lanzar la peticiones http.
+    private TestRestTemplate testRestTemplate;
+
+    // se coloca la notación @Autowired para indicarle a Spring que nos inyecte este Builder
+    @Autowired
+    private RestTemplateBuilder restTemplateBuilder;
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    void setUp() {
+        restTemplateBuilder = restTemplateBuilder.rootUri("http://localhost:" + port);
+        testRestTemplate = new TestRestTemplate(restTemplateBuilder);
+    }
+
+    @Test
+    void holaMundo() {
+        ResponseEntity<String> response =
+                testRestTemplate.getForEntity("/hola", String.class);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(200, response.getStatusCodeValue());
+        assertEquals("hola Mundo", response.getBody());
+    }
+
+    @Test
+    void findAll() {
+        testRestTemplate.getForEntity("/api/books", List.class);
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void save() {
+    }
+}
